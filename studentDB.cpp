aeadd3f361d15dd67fc3dc923360130dea0e837a
aadityad12 <aaditya.d.desai@gmail.com>
2025-03-01 10:49:36 +0530

    Update and rename main.cpp to studentDB.cpp

--- studentDB.cpp
+++ studentDB.cpp
@@ -1,8 +1,3 @@
-// Date: Jan 28
-/* Description: This code accepts the num of students, each of their names and
-then the number of tests each student takes it then also accepts the grade for
-each of the tests. All of this data is stored in a heap and all the steps are
-divided into 3 function. */ 
 #include <iostream>
 
 using namespace std;
@@ -95,27 +90,3 @@ void cleanHeap(string* names, double** grades, int numStudents) {
     delete[] grades;
     delete[] names;
 }
- 
-/* Output
-Enter the number of students: 3
-Enter the student's name: bob
-Enter how many tests bob took: 3
-Enter grade #1 100
-Enter grade #2 99.5
-Enter grade #3 65
-Enter the student's name: dev
-Enter how many tests dev took: -3
-Enter how many tests dev took: 2
-Enter grade #1 10.5
-Enter grade #2 85
-Enter the student's name: sam
-Enter how many tests sam took: 1
-Enter grade #1 100 
-You have 3 students in the system.
-Name of student #1: bob
-Grades for student #1: 100 99.5 65 
-Name of student #2: dev
-Grades for student #2: 10.5 85 
-Name of student #3: sam
-Grades for student #3: 100 
-*/
